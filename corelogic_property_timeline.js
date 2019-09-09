8a2a7540d35bd2441a354dbfaf372eb17deb7a45
Juan Gil <jugil@corelogic.com>
2019-09-09 12:02:26 -0500

    testing iframe

--- corelogic_property_timeline.js
+++ corelogic_property_timeline.js
@@ -406,6 +406,16 @@
 				.fa-file-pdf-o:before{
 					content:"\f1c1"
 				}
+				.box{
+					display: none;
+					width: 100%;
+				}
+
+				a:hover + .box,.box:hover{
+					display: block;
+					position: relative;
+					z-index: 100;
+				}
             `
         },
 
@@ -764,7 +774,7 @@
                 .on('mouseover', d => {
                     if (d[columns.event_hover]) {
                         vis.cancelHideTip()
-                        vis.ui.tip.innerHTML = d[columns.event_hover]
+                        vis.ui.tip.innerHTML = d[columns.event_hover] + '<div class="box"><iframe src="https://vizcenter.corelogic.net/visual/assets/11231000029.pdf" width = "500px" height = "500px"></iframe></div>'
                         const tip = vis.ui.tip.getBoundingClientRect()
                         const pane = vis.ui.pane.getBoundingClientRect()
                         let left = d3.event.pageX + 10
